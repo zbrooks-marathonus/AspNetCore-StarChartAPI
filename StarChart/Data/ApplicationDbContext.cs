768edd2dbc4bc6b53ff7322bc16611ecfce13957
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-22 08:32:54 -0700

    Middlewear config

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> celestialObjects { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            
         }
     }
 }
---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satallites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
