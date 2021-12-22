3f8a3ab4767636fe65bc34e341e28d411cb7ddce
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-22 09:18:02 -0700

    Updated to Controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -14,5 +19,70 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                CelestialObject CelObject = _context.CelestialObjects.FirstOrDefault(i => i.Id == id);
+                if (CelObject == null) return NotFound();
+
+                CelObject.Satellites = _context.CelestialObjects
+                                        .Where(i => i.OrbitedObjectId == CelObject.Id)
+                                        .ToList<CelestialObject>();
+                return Ok(CelObject);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Unable to process request");
+            }
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            try
+            {
+                List<CelestialObject> CelObject = _context.CelestialObjects
+                        .Where(i => i.Name == name)
+                        .ToList<CelestialObject>();
+                 if (CelObject.Count() == 0) return NotFound();
+
+                foreach (var CelestialObject in CelObject)
+                {
+                    CelestialObject.Satellites = _context.CelestialObjects
+                        .Where(i => i.OrbitedObjectId == CelestialObject.Id)
+                        .ToList<CelestialObject>();
+                }
+
+                return Ok(CelObject);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Unable to process request");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                List<CelestialObject> objectList = _context.CelestialObjects.ToList<CelestialObject>();
+
+                foreach (var CelestialObject in objectList)
+                {
+                    CelestialObject.Satellites = _context.CelestialObjects
+                        .Where(i => i.OrbitedObjectId == CelestialObject.Id)
+                        .ToList<CelestialObject>();
+                }
+
+                return Ok(objectList);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Unable to process request");
+            }
+        }
     }
 }
