c3864d40a49b3bf1107c20a29ff5cbf3cff594cb
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-22 08:34:48 -0700

    Update ApplicationDbContext.cs

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<CelestialObject> celestialObjects { get; set; }
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
